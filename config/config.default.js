86787e2ad8ad03133dbcafc46c64b1dbf734f5da
free123abcsky <630353983@qq.com>
2018-05-26 17:05:42 +0800

    初始化

--- config/config.default.js
+++ config/config.default.js
@@ -55,11 +55,11 @@ module.exports = appInfo => {
     }
   }
 
-  // config.cors= {
-  //     origin: '*',
-  //    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
-  //    credentials: true
-  // }
+  config.cors= {
+      origin: '*',
+     allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
+     credentials: true
+  }
 
   return config
 }
